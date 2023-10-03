a81942f20c57a8165d638680f18dd9627162be9a
Vianckieeee <142567906+Vianckieeee@users.noreply.github.com>
2023-10-03 18:08:32 +0800

    Create assignmentoperators.js

---
+++ assignmentoperators.js
@@ -0,0 +1,14 @@
+/*
+      Assignment operators in JavaScript are used to assign values to variables. 
+      They combine the operation of assigning a value to a variable with a specific operation, like addition or subtraction. 
+    */
+
+    let year = 2050;
+    let newYear = year = 2051;
+
+    // This is also the same as this:
+    /*
+      let year = 2050;
+      year = 2051;
+      let newYear = year;
+    */
