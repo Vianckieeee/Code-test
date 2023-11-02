cf7361afd05c9e024ae21f587f0c82fdd37334c6
Vianckieeee <142567906+Vianckieeee@users.noreply.github.com>
2023-11-02 10:04:51 +0800

    4.0.3 the if else statement.js

---
+++ 4.0.3 the if else statement.js
@@ -0,0 +1,14 @@
+ /*
+    the if else syntax:
+      if (condition) {
+    condition - true code
+  } else {
+    condition - false code
+  }
+    */ 
+      let isUserReady = confirm("Are you ready?");
+  if (isUserReady) {
+      console.log("User ready!");
+  } else {
+      console.log("User not ready!");
+  }
