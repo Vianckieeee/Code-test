cb78da0e0313bbf44de0b82474b62a193b1f746a
Vianckieeee <142567906+Vianckieeee@users.noreply.github.com>
2023-11-24 19:01:30 +0800

    Create 4.1.8 continue and break.js

---
+++ 4.1.8 continue and break.js
@@ -0,0 +1,18 @@
+let i = 0;
+  // An infinite loop
+  while (true){
+      console.log(i);
+      i++;
+      if (i >= 5) {
+      break;
+      }
+  }
+  alert(`Exited the loop with a break (${i}).`);
+
+  console.log("++++++++++++++++++++");
+  for (let i = 0; i < 10; i++) {
+      if (i == 3) {
+      continue;
+      }
+      console.log(i);
+  }
