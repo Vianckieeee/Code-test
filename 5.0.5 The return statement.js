a1c7eff142679f07b9b6b43c82ad5aec07907618
Vianckieeee <142567906+Vianckieeee@users.noreply.github.com>
2023-12-01 11:27:36 +0800

    Create 5.0.5 The return statement.js

---
+++ 5.0.5 The return statement.js
@@ -0,0 +1,6 @@
+/*
+What exactly is return for?
+- First, it causes the function to end exactly where this word occurs, even if there are further instructions.
+- Second, it allows us to return a given value from inside the function to the place where it was called.
+*/ 
+
