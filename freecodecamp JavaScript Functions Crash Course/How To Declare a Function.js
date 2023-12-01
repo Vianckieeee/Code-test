10099f0be0663c6dd4c4280fd412dd3636e735b4
Vianckieeee <142567906+Vianckieeee@users.noreply.github.com>
2023-12-01 13:31:31 +0800

    Update How To Declare a Function.js

--- freecodecamp JavaScript Functions Crash Course/How To Declare a Function.js
+++ freecodecamp JavaScript Functions Crash Course/How To Declare a Function.js
@@ -1,1 +1,37 @@
+// Declaring function without expression
+function printMe(){
+  console.log('printing...');
+}
 
+console.log('++++++++++++++++++');
+
+// Parameter is anything you declared on the function definition
+function printThis(params){
+  console.log(params);
+}
+// This will print Penguin. This is called Argument.
+// If you invoke or called the function, 
+// the actual value that you will pass to the function is called argument
+printThis('Penguin');
+
+console.log('++++++++++++++++++');
+
+// Declaring a function using function expressions
+const printMeAgain = function(){
+     console.log('printing...');
+ }
+
+printMeAgain();
+
+console.log('++++++++++++++++++');
+const printMeAgainWithParam = function(a, b){
+    console.log(a, b);
+}
+
+printMeAgainWithParam('Vien', 'Uy');
+
+console.log('++++++++++++++++++');
+const GetTwoNum = function(a, b){
+  console.log(a, b);
+}
+GetTwoNum(21,11);
