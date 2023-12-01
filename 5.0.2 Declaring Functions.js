6283af7092792dc248550196295073f32d6c7bce
Vianckieeee <142567906+Vianckieeee@users.noreply.github.com>
2023-12-01 10:06:46 +0800

    Create 5.0.2 Declaring Functions.js

---
+++ 5.0.2 Declaring Functions.js
@@ -0,0 +1,18 @@
+/*
+Functions syntax:
+function functionName() {
+   code
+}
+*/ 
+
+let temperatures;
+let sum;
+let meanTemp;
+ 
+function getMeanTemp() {
+     sum = 0;
+     for (let i = 0; i < temperatures.length; i++) {
+         sum += temperatures[i];
+     }
+     meanTemp = sum / temperatures.length;    
+}
