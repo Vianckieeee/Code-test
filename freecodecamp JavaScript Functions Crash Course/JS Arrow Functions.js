48d51cfa320192b4ef2dd17494c7d56fdf060412
Vianckieeee <142567906+Vianckieeee@users.noreply.github.com>
2023-12-01 18:45:36 +0800

    Create JS Arrow Functions.js

---
+++ freecodecamp JavaScript Functions Crash Course/JS Arrow Functions.js
@@ -0,0 +1,11 @@
+/*
+Normal way syntax of making a function expressions
+*/
+const add = function(x,y) {
+    return x + y;
+}
+
+/*
+Code wt Arrow function expression
+*/ 
+const add = (x,y) => x + y;
