7c969721b9cd41edd84538db67b6a4ac11c33e27
Vianckieeee <142567906+Vianckieeee@users.noreply.github.com>
2023-12-03 11:40:31 +0800

    Update JS Closure Function.js

--- freecodecamp JavaScript Functions Crash Course/JS Closure Function.js
+++ freecodecamp JavaScript Functions Crash Course/JS Closure Function.js
@@ -1,1 +1,31 @@
-//GG
+// 
+
+function outerFunction() {
+  let outerVariable = "I am from outerFunction";
+
+  function innerFunction() {
+    console.log(outerVariable);
+  }
+
+  return innerFunction;
+}
+
+// Create a closure by calling outerFunction
+let closure = outerFunction();
+
+// Call the closure, and it still has access to outerVariable
+closure(); // Output: "I am from outerFunction"
+
+/*
+Explanation:
+1. outerFunction is defined, and it contains a variable called outerVariable and an 
+inner function called innerFunction
+
+2. outerFunction is invoked, and it returns innerFunction (without calling it).
+3.The returned innerFunction is assigned to the variable closure.
+4. When we later call closure(), it still has access to the outerVariable
+ even though outerFunction has finished executing. This is possible because innerFunction forms a closure, 
+ "capturing" the lexical environment in which it was created.
+
+ 
+*/ 
